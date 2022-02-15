f245ffe015e144eb26559c223de457fcd27ef1b6
jongmin <ace4463@gmail.com>
2022-02-15 18:00:29 +0900

    삼각형 판별하기

--- src/solution1.js
+++ src/solution1.js
@@ -1,8 +1,9 @@
-function solution(a, b, c){
+//3개 굿자 중 최소값 구하기
+function solution(a, b, c) {
     let answer;
-    if(a<b) answer=a;
-    else answer=b;
-    if(c<answer) answer=c;
+    if (a < b) answer = a;
+    else answer = b;
+    if (c < answer) answer = c;
     return answer;
 }
 
---
+++ src/solution2.js
@@ -0,0 +1,12 @@
+//삼각형 판별
+function solution(a, b, c) {
+    let answer = "YES", max;
+    let tot = a + b + c;
+    if (a > b) max = a;
+    else max = b;
+    if (c > max) max = c;
+    if (tot - max <= max) answer = "NO";
+    return answer;
+}
+
+console.log(solution(13, 33, 17));
