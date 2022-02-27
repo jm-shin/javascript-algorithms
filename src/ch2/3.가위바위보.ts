aedd546039045bfdd4e0df47dd2d50301c7c9858
jongminshin <ace4463@gmail.com>
2022-02-27 11:59:23 +0900

    ch2-4

--- src/ch2/3.가위바위보.ts
+++ src/ch2/3.가위바위보.ts
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-function solution(a: number[], b:number[]){
-    let answer="";
-    for(let i=0; i<a.length; i++){
-        if(a[i]===b[i]) answer+="D ";
-        else if(a[i]===1 && b[i]===3) answer+="A ";
-        else if(a[i]===2 && b[i]===1) answer+="A ";
-        else if(a[i]===3 && b[i]===2) answer+="A ";
-        else answer+="B ";
-    }
-    return answer;
-}
-
-let a=[2, 3, 3, 1, 3];
-let b=[1, 1, 2, 2, 3];
-console.log(solution(a, b));
-=======
 function solution(a: number[], b: number[]): string[] {
     return a.reduce((acc, cur, i) => {
         if (cur === b[i]) {
@@ -44,4 +27,3 @@ function solution2(a: number[], b: number[]): string[] {
 const a = [2, 3, 3, 1, 3];
 const b = [1, 1, 2, 2, 3];
 console.log(solution2(a, b));
->>>>>>> 7640195b88335f61e408456c98effae1a75503d1
