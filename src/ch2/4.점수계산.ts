629f83c4101cc2d41e18b07e4102da837f9ff0ea
jongminshin <ace4463@gmail.com>
2022-02-28 15:12:06 +0900

    ch2-4

--- src/ch2/4.점수계산.ts
+++ src/ch2/4.점수계산.ts
@@ -1,15 +1,16 @@
-function solution(arr: number[]){
-    let answer=0, cnt=0;
-    for(let x of arr){
-        if(x===1){
-            cnt++;
-            answer+=cnt;
+function solution(array: number[]){
+    let answer = 0;
+    let count = 0;
+    for (let x of array) {
+        if (x === 1) {
+            count ++;
+            answer += count;
+        } else if (x === 0) {
+            count = 0;
         }
-        else cnt=0;
     }
-
     return answer;
 }
 
-let arr=[1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
-console.log(solution(arr));
+let array = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(array));
