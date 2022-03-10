8fb15df58d96f68e6fe0fb1514304a31452fc5c4
jongmin <ace4463@gmail.com>
2022-03-10 19:04:40 +0900

    ch3-2

--- src/ch3/2.유효한팰린드롬.ts
+++ src/ch3/2.유효한팰린드롬.ts
@@ -1,9 +1,9 @@
-function solution(s){
-    let answer="YES";
-    s=s.toLowerCase().replace(/[^a-z]/g, '');
-    if(s.split('').reverse().join('')!==s) return "NO";
+function solution(str: string) {
+    let answer = "YES";
+    str = str.toLowerCase().replace(/[^a-z]/g, '');
+    if (str.split('').reverse().join('') !== str) return "NO";
     return answer;
 }
 
-let str="found7, time: study; Yduts; emit, 7Dnuof";
+let str = "found7, time: study; Yduts; emit, 7Dnuof";
 console.log(solution(str));
