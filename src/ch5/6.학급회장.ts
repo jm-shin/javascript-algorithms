dc8dd1da2949df652f4dc5dc0f623348edc06a9b
jongminshin <ace4463@gmail.com>
2022-05-29 14:02:47 +0900

    Add ch5-7

---
+++ src/ch5/6.학급회장.ts
@@ -0,0 +1,19 @@
+function solution(s: string) {
+  let answer;
+  let sH = new Map() as any;
+  for (let x of s) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
+  }
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let [key, val] of sH) {
+    if (val > max) {
+      max = val;
+      answer = key;
+    }
+  }
+  return answer;
+}
+
+let str = "BACBACCACCBDEDE";
+console.log(solution(str));
